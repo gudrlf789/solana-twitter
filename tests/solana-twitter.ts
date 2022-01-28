5b14afd83690e019611f252dce80d55cf07c1676
park hyeong gil <gudrlf7913@naver.com>
2022-01-28 15:42:39 +0900

    topic 없는 시나리오 테스트 코드

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -5,22 +5,25 @@ import * as assert from "assert";
 
 describe('solana-twitter', () => {
 
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it('can send a new tweet', async () => {
+  it('can send a new tweet without a topic', async () => {
     const tweet = anchor.web3.Keypair.generate();
-    await program.rpc.sendTweet('veganism', 'Hummus, am I right?', {
-      accounts: {
-        tweet: tweet.publicKey,
-        author: program.provider.wallet.publicKey,
-        systemProgram: anchor.web3.systemProgram.programId,
-      },
-      signers: [tweet],
+    await program.rpc.sendTweet('', 'gm', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
     });
 
     const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
-    console.log(tweetAccount);
+
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, '');
+    assert.equal(tweetAccount.content, 'gm');
+    assert.ok(tweetAccount.timestamp);
   });
 });
